7dce07a71119b688d28dff293e971717499c7d87
Andrey <lom16133@gmail.com>
2014-11-05 22:21:35 +0300

    Documentation update

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,15 +2,17 @@
 #include <QApplication>
 #include <QPixmap>
 #include <QSplashScreen>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QPixmap pixmap(":/logo/logo.png");
+    QPixmap pixmap(":/logo/splash.png");
     QSplashScreen splash(pixmap);
     splash.show();
-    a.processEvents();
     Window w;
-    w.show();
+    QTimer::singleShot(2500, &splash, SLOT(close()));
+    QTimer::singleShot(2500, &w, SLOT(show()));
+    //w.show();
     return a.exec();
 }
