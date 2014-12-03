3bd9c1d2d75df8727f85b528c2b0ad6fe301992b
Grandbrain <lom16133@gmail.com>
2014-12-03 17:49:15 +0300

    Some improvements

--- Source/Sensor.pro
+++ Source/Sensor.pro
@@ -7,11 +7,10 @@ CONFIG += c++11
 TARGET = Sensor
 TEMPLATE = app
 
+LIBS += -lws2_32
 
-SOURCES 	+= main.cpp window.cpp sensor.cpp about.cpp \
-    search.cpp
-HEADERS  	+= sensor.h window.h about.h \
-    search.h
-FORMS    	+= window.ui about.ui \
-    search.ui
+
+SOURCES 	+= main.cpp window.cpp sensor.cpp about.cpp search.cpp
+HEADERS  	+= sensor.h window.h about.h search.h
+FORMS    	+= window.ui about.ui search.ui
 RESOURCES 	+= resources.qrc
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,18 +1,7 @@
-#include "window.h"
 #include <QApplication>
-#include <QPixmap>
-#include <QSplashScreen>
-#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QPixmap pixmap(":/logo/splash.png");
-    QSplashScreen splash(pixmap);
-    splash.show();
-    Window w;
-    QTimer::singleShot(2500, &splash, SLOT(close()));
-    QTimer::singleShot(2500, &w, SLOT(show()));
-    //w.show();
     return a.exec();
 }
